aca24f7075bcac132a03dbfc052b6ce5be8a4e79
lucasbarroscode <lucasbabezerra@gmail.com>
2025-02-26 21:43:42 -0300

    criando o projeto e montando as models

---
+++ pedidos-api/src/main/java/br/com/lucascode/pedidos/api/entity/ItemPedido.java
@@ -0,0 +1,18 @@
+package br.com.lucascode.pedidos.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemPedido {
+
+    private UUID id = UUID.randomUUID();
+    private Produto produto;
+    private Integer quantidade;
+
+}
---
+++ pedidos-api/src/main/java/br/com/lucascode/pedidos/api/enums/Status.java
@@ -0,0 +1,8 @@
+package br.com.lucascode.pedidos.api.enums;
+
+public enum Status {
+
+    EM_PROCESSAMENTO, PROCESSADO;
+
+
+}

20ea149a73346df2ee0d9bd5814ed2c5faa972b0
lucasbarroscode <lucasbabezerra@gmail.com>
2025-03-06 00:37:45 -0300

    add service in controller

--- pedidos-api/src/main/java/br/com/lucascode/pedidos/api/controller/PedidoController.java
+++ pedidos-api/src/main/java/br/com/lucascode/pedidos/api/controller/PedidoController.java
@@ -1,6 +1,7 @@
 package br.com.lucascode.pedidos.api.controller;
 
 import br.com.lucascode.pedidos.api.entity.Pedido;
+import br.com.lucascode.pedidos.api.service.PedidoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -16,9 +17,16 @@ public class PedidoController {
 
     private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
 
+    private final PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
     @PostMapping
     public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido){
         logger.info("Pedido recebido: {}", pedido.toString());
+        pedido = pedidoService.enfileirarPedido(pedido);
         return ResponseEntity.status(HttpStatus.CREATED).body(pedido);
     }
 

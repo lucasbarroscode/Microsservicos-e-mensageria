ce214ad3c53a86bda4435390b275eaaa790a6274
lucasbarroscode <lucasbabezerra@gmail.com>
2025-03-06 00:55:17 -0300

    criando modulo de notificacao pedido

---
+++ notificacao/src/main/java/br/com/lucascode/pedidos/notificacao/notificacao/NotificacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucascode.pedidos.notificacao.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoApplication.class, args);
+	}
+
+}

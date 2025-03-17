3a5a82a6951cbd37ff56e97c77d85344a37c99eb
lucasbarroscode <lucasbabezerra@gmail.com>
2025-03-17 00:44:22 -0300

    enviando a notificacao por email

---
+++ notificacao/src/main/java/br/com/lucascode/pedidos/notificacao/notificacao/service/EmailService.java
@@ -0,0 +1,36 @@
+package br.com.lucascode.pedidos.notificacao.notificacao.service;
+
+import br.com.lucascode.pedidos.notificacao.notificacao.entity.Pedido;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
+
+    public void enviarEmail(Pedido pedido){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("pedidos-api@compony.com");
+        simpleMailMessage.setTo(pedido.getEmailNotificacao());
+        simpleMailMessage.setSubject("Pedido de Compra");
+        simpleMailMessage.setText(this.gerarMensagem(pedido));
+        mailSender.send(simpleMailMessage);
+
+    }
+
+    private String gerarMensagem(Pedido pedido) {
+        String pedidoId = pedido.getId().toString();
+        String cliente = pedido.getCliente();
+        String valor = String.valueOf(pedido.getValorTotal());
+        String status = pedido.getStatus().name();
+
+        return String.format("Olá %s seu pedido de n° %s, no valor de %s foi realizado com sucesso. \n Status: %s", cliente, pedidoId,valor,status);
+
+    }
+
+}

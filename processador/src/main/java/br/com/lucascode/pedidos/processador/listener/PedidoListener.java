3c50fdefe61bf18521cf74426b24e144da297654
lucasbarroscode <lucasbabezerra@gmail.com>
2025-04-30 11:26:52 -0300

    criando listener para processamento

---
+++ processador/src/main/java/br/com/lucascode/pedidos/processador/listener/PedidoListener.java
@@ -0,0 +1,21 @@
+package br.com.lucascode.pedidos.processador.listener;
+
+import br.com.lucascode.pedidos.processador.entity.Pedido;
+import br.com.lucascode.pedidos.processador.entity.enums.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PedidoListener {
+
+    private final Logger logger = LoggerFactory.getLogger(PedidoListener.class);
+
+    @RabbitListener(queues = "pedidos.v1.pedido-criado.gerar-processamento")
+    public void salvarPedido(Pedido pedido){
+        pedido.setStatus(Status.PROCESSADO);
+        logger.info("Pedido Processado: {}", pedido.toString());
+    }
+
+}

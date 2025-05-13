48d458bbdab76c11c3e4a0483437e8f396dc95b0
lucasbarroscode <lucasbabezerra@gmail.com>
2025-05-12 22:23:49 -0300

    configurando fila de pedidos não processados

--- notificacao/src/main/java/br/com/lucascode/pedidos/notificacao/notificacao/config/RabbitMQConfig.java
+++ notificacao/src/main/java/br/com/lucascode/pedidos/notificacao/notificacao/config/RabbitMQConfig.java
@@ -15,6 +15,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RabbitMQConfig {
 
@@ -24,33 +27,56 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.queue.name}")
     private String queueName;
 
+    @Value("${rabbitmq.exchange.dlx.name}")
+    private String exchangeDlxName;
+
+    @Value("${rabbitmq.queue.dlq.name}")
+    private String queueDlqName;
+
     @Bean
     public FanoutExchange pedidosExchange() {
         return new FanoutExchange(exchangeName);
     }
 
     @Bean
-    public Queue notificacaoQueue() {
-       return new Queue(queueName);
+    public FanoutExchange pedidosDlxExchange() {
+        return new FanoutExchange(exchangeDlxName);
     }
 
     @Bean
-    public Binding binding(){
+    public Queue notificacaoQueue() {
+        Map<String, Object> argumentos = new HashMap<>();
+        argumentos.put("x-dead-letter-exchange", exchangeDlxName);
+        return new Queue(queueName, true, false, false, argumentos);
+    }
+
+    @Bean
+    public Queue notificacaoDlqQueue() {
+        return new Queue(queueDlqName);
+    }
+
+    @Bean
+    public Binding binding() {
         return BindingBuilder.bind(notificacaoQueue()).to(pedidosExchange());
     }
 
     @Bean
-    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+    public Binding bindingDlq() {
+        return BindingBuilder.bind(notificacaoDlqQueue()).to(pedidosDlxExchange());
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
         return new RabbitAdmin(connectionFactory);
     }
 
     @Bean
-    public MessageConverter messageConverter(){
+    public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter){
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
@@ -60,6 +86,5 @@ public class RabbitMQConfig {
     public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener(RabbitAdmin rabbitAdmin) {
         return event -> rabbitAdmin.initialize();
     }
-
 
 }
